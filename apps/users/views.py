7bec6f4f9bb1eb6d80cd6c1989292cc1aa63d9e1
dtgamer <144635041+dtgamer@users.noreply.github.com>
2023-09-26 03:58:56 +0100

    Update views.py

--- apps/users/views.py
+++ apps/users/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.exceptions import NotFound
 from django.shortcuts import redirect
 from rest_framework.authentication import TokenAuthentication
 from django.shortcuts import render
