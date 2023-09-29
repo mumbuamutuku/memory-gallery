4f9996c2d7a7f3f7150fefbc0adddfda8ad73dae
Abakpa Dominic <abakpad82@gmail.com>
2023-09-29 10:34:44 +0100

    Update models.py

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,20 +4,28 @@ from django.utils import timezone
 from django.conf import settings
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, username, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
+        if not username:
+            raise ValueError('The Username field must be set')
+
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(email, password, **extra_fields)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, username, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
@@ -36,7 +44,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     class Meta:
         unique_together = ('email', 'username')
-
+        
 class UserProfile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, 
