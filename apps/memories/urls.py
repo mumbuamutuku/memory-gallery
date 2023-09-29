ed6ee508b98c2c35f12b13d319adde50c6721167
Abakpa Dominic <abakpad82@gmail.com>
2023-09-29 11:11:19 +0100

    updated files

--- apps/memories/urls.py
+++ apps/memories/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.urls import path
 from .views import AlbumListCreateView, AlbumDetailView, MemoryListCreateView, MemoryDetailView
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('albums/', AlbumListCreateView.as_view(), name='album-list-create'),
@@ -7,3 +9,5 @@ urlpatterns = [
     path('memory/', MemoryListCreateView.as_view(), name='memory-list-create'),
     path('memory/<int:pk>/', MemoryDetailView.as_view(), name='memory-detail'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
