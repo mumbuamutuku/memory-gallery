63dfdc8c495835f38da0f92578992919b8fc2fc2
Abakpa Dominic <abakpad82@gmail.com>
2023-09-29 10:32:49 +0100

    Update urls.py

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login/', UserLoginAPIView.as_view(), name='user-login'),
     path('profile/<int:pk>/', UserProfileAPIView.as_view(), name='user-profile'),
     path('edit-profile/', EditProfileView.as_view(), name='edit-profile'),  # New endpoint for editing profile
-    path('create-profile/', CreateProfileView.as_view(), name='create-profile'),
+    #path('create-profile/', CreateProfileView.as_view(), name='create-profile'),
     path('logout/', LogoutView.as_view(), name='user-logout'),
 ]
 if settings.DEBUG:
