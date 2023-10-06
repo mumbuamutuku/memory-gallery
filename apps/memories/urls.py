c9efcef8c371cf99d087e6879726638aec0cbc8c
dtgamer <144635041+dtgamer@users.noreply.github.com>
2023-10-06 23:26:38 +0100

    Update urls.py

--- apps/memories/urls.py
+++ apps/memories/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import AlbumListCreateView, AlbumDetailView, MemoryListCreateView, MemoryDetailView
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('albums/', AlbumListCreateView.as_view(), name='album-list-create'),
     path('albums/<int:pk>/', AlbumDetailView.as_view(), name='album-detail'),
+    path('albums/<int:album_id>/memories/', views.MemoriesByAlbumView.as_view(), name='memories-by-album'),
     path('memory/', MemoryListCreateView.as_view(), name='memory-list-create'),
     path('memory/<int:pk>/', MemoryDetailView.as_view(), name='memory-detail'),
 ]
